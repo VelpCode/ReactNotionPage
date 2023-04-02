927e62ef18e4a602e75ea415d8f54dedb7d4d92e
Aman Velpula <velpmail@gmail.com>
2023-04-02 14:58:36 -0400

    experiemenitng with react useEffect and useState

--- reactnotion/src/App.js
+++ reactnotion/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return(
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert("You've changed the counter");
+  }, [counter]);
+
   return (
     <div className="App">
-      <Person name = "John" lastName='Doe' age = {25} />
-      <Person name = "Mary" lastName='Kent' />
-      
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
