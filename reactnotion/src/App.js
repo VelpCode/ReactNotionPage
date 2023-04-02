b3c5bc2378ddc10ce812ab751e412619c9596304
Aman Velpula <velpmail@gmail.com>
2023-04-02 14:36:12 -0400

    testing react props

--- reactnotion/src/App.js
+++ reactnotion/src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Person = (props) => {
+  return(
+    <>
+    <h1>Name: {props.name}</h1>
+    <h2>Last Name: {props.lastName}</h2>
+    <h2>Age: {props.age}</h2>
+    </>
+  )
+}
+
 function App() {
-  const name = "Daarla Phrabu"
   return (
     <div className="App">
-      <h1>Web {name} Template Website</h1 >
+      <Person name = "John" lastName='Doe' age = {25} />
+      <Person name = "Mary" lastName='Kent' />
+      
     </div>
   );
 }
